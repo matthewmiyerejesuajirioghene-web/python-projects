a0be95a32086350c49aa6c690171db574ec41c60
Matthew Ajiri <matthewmiyerejesuajirioghene@gmail.com>
2026-08-25 12:51:17 +0100

    Describe what I changed

---
+++ PROJECT/modules2.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+# Absolute path
+# Relative path
+
+path = Path("emails")
+print(path.rmdir())
---
+++ package.py
@@ -0,0 +1,5 @@
+# a package is a container for multiple modules/ directories
+# a package can also be referred to as a folder
+
+from ecommerce.shipping import shipping
+calc_shipping()
